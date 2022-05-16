d5d512d4507baa160a83623dcc61cb908096ed46
Soboroo <woo036325@gmail.com>
2022-05-16 23:42:30 +0900

    Using MS snipping tool when it get a screenshot

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -9,6 +9,12 @@ using Windows.Storage.Streams;
 using Windows.Graphics.Imaging;
 using Windows.ApplicationModel.DataTransfer;
 using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using Windows.UI.Core;
+using System.Diagnostics;
+using System.Collections.Generic;
+using Windows.Storage;
+using uwp_qr_demo.Utils;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -17,17 +23,10 @@ namespace uwp_qr_demo
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    /// 
-    [ComImport]
-    [Guid("5B0D3235-4DBA-4D44-865E-8F1D0E4FD04D")]
-    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-    unsafe interface IMemoryBufferByteAccess
-    {
-        void GetBuffer(out byte* buffer, out uint capacity);
-    }
-
     public sealed partial class MainPage : Page
     {
+        private DataPackage data;
+
         public MainPage()
         {
             this.InitializeComponent();
@@ -41,55 +40,124 @@ namespace uwp_qr_demo
             // Set XAML element as a drag region.
             Window.Current.SetTitleBar(AppTitleBar);
             AppTitleTextBlock.Text = AppInfo.Current.DisplayInfo.DisplayName;
-
-            OpenCvSharp.Mat mat;
-            string qrResult = "";
-            Clipboard.ContentChanged += async (s, e) =>
-            {
-                try
-                {
-                    DataPackageView dataPackageView = Clipboard.GetContent();
-                    string copiedFormat = dataPackageView.AvailableFormats.FirstOrDefault();
-                    if (copiedFormat == "Bitmap")
-                    {
-                        // Get QR code from clipboard using OpenCV
-                        RandomAccessStreamReference bitmap = await dataPackageView.GetBitmapAsync();
-                        IRandomAccessStream stream = await bitmap.OpenReadAsync();
-                        BitmapDecoder decoder = await BitmapDecoder.CreateAsync(stream);
-                        SoftwareBitmap softwareBitmap = await decoder.GetSoftwareBitmapAsync();
-
-                        mat = SoftwareBitmap2Mat(softwareBitmap);
-
-                        OpenCvSharp.QRCodeDetector qRCodeDetector = new OpenCvSharp.QRCodeDetector();
-                        OpenCvSharp.Point2f[] points;
-                        qrResult = qRCodeDetector.DetectAndDecode(mat, out points);
-
-                        // Display QR code result
-                        if (qrResult != "")
-                        {
-                            QrResultTextBlock.Text = qrResult;
-                        }
-                    }
-                }
-                catch (Exception ex)
-                {
-                    qrResult = "";
-                }
-            };
         }
-        // https://github.com/shimat/opencvsharp_samples/blob/master/CameraOpenCV/OcvOp.cs
-        public static unsafe OpenCvSharp.Mat SoftwareBitmap2Mat(SoftwareBitmap softwareBitmap)
+        
+        private async void ScanButton_Click(object sender, RoutedEventArgs e)
         {
-            using (BitmapBuffer buffer = softwareBitmap.LockBuffer(BitmapBufferAccessMode.Write))
+            DataPackageView currentClipboard = Clipboard.GetContent();
+            if (currentClipboard != null)
             {
-                using (var reference = buffer.CreateReference())
-                {
-                    ((IMemoryBufferByteAccess)reference).GetBuffer(out var dataInBytes, out var capacity);
+                data = await DataPackageViewToDataPackage(currentClipboard);
+            }
+            Clipboard.Clear();
+            bool result = await Windows.System.Launcher.LaunchUriAsync(new Uri("ms-screenclip:snip?source=qr_demo"));
+            if (result)
+            {
+                Window.Current.Activated += SnippingToolOpened;
+            }
+            else
+            {
+                // Failed to launch screen clip (different from didn't take a screenshot)
+            }
+        }
 
-                    OpenCvSharp.Mat outputMat = new OpenCvSharp.Mat(softwareBitmap.PixelHeight, softwareBitmap.PixelWidth, OpenCvSharp.MatType.CV_8UC4, (IntPtr)dataInBytes);
-                    return outputMat;
+        private void SnippingToolOpened(object sender, WindowActivatedEventArgs e)
+        {
+            if (e.WindowActivationState != CoreWindowActivationState.Deactivated)
+            {
+                DataPackageView dataPackageView = Clipboard.GetContent();
+                if (dataPackageView.Contains(StandardDataFormats.Bitmap))
+                {
+                    Debug.WriteLine("Screenshot copied to clipboard");
+                    GetQRInfo(Clipboard.GetContent());
+                }
+                Window.Current.Activated -= SnippingToolOpened;
+            }
+        }
+
+        private async void GetQRInfo(DataPackageView clipboard)
+        {
+            if (clipboard.Contains(StandardDataFormats.Bitmap))
+            {
+                RandomAccessStreamReference bitmap = await clipboard.GetBitmapAsync();
+                string result = await QRDecodeHelper.GetQRInfoFromBitmap(bitmap);
+                Clipboard.SetContent(data);
+                if (result != null && result.Length > 0)
+                {
+                    var dialog = new ContentDialog
+                    {
+                        Title = "QR Code",
+                        Content = result,
+                        PrimaryButtonText = "Copy",
+                        CloseButtonText = "Close"
+                    };
+
+                    dialog.PrimaryButtonClick += (s, e) =>
+                    {
+                        data.SetText(result);
+                        Clipboard.SetContent(data);
+                    };
+
+                    await dialog.ShowAsync();
+                }
+                else
+                {
+                    var dialog = new ContentDialog
+                    {
+                        Title = "QR Code",
+                        Content = "No QR Code found",
+                        CloseButtonText = "Close"
+                    };
+                    await dialog.ShowAsync();
                 }
             }
+            else
+            {
+                var dialog = new ContentDialog
+                {
+                    Title = "QR Code",
+                    Content = "Given Data is not a bitmap",
+                    CloseButtonText = "Close"
+                };
+                await dialog.ShowAsync();
+            }
+            
+        }
+
+        private async Task<DataPackage> DataPackageViewToDataPackage(DataPackageView dataPackageView)
+        {
+            DataPackage dataPackage = new DataPackage();
+            if (dataPackageView.Contains(StandardDataFormats.Bitmap))
+            {
+                RandomAccessStreamReference bitmap = await dataPackageView.GetBitmapAsync();
+                dataPackage.SetBitmap(bitmap);
+            }
+            else if (dataPackageView.Contains(StandardDataFormats.Text))
+            {
+                string text = await dataPackageView.GetTextAsync();
+                dataPackage.SetText(text);
+            }
+            else if (dataPackageView.Contains(StandardDataFormats.Html))
+            {
+                string html = await dataPackageView.GetHtmlFormatAsync();
+                dataPackage.SetHtmlFormat(html);
+            }
+            else if (dataPackageView.Contains(StandardDataFormats.Rtf))
+            {
+                string rtf = await dataPackageView.GetRtfAsync();
+                dataPackage.SetRtf(rtf);
+            }
+            else if (dataPackageView.Contains(StandardDataFormats.StorageItems))
+            {
+                IReadOnlyList<IStorageItem> storageItems = await dataPackageView.GetStorageItemsAsync();
+                dataPackage.SetStorageItems(storageItems);
+            }
+            else if (dataPackageView.Contains(StandardDataFormats.WebLink))
+            {
+                Uri webLink = await dataPackageView.GetWebLinkAsync();
+                dataPackage.SetWebLink(webLink);
+            }
+            return dataPackage;
         }
     }
 }
---
+++ Utils/QRDecodeHelper.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Graphics.Imaging;
+using Windows.Storage.Streams;
+
+namespace uwp_qr_demo.Utils
+{
+    [ComImport]
+    [Guid("5B0D3235-4DBA-4D44-865E-8F1D0E4FD04D")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    unsafe interface IMemoryBufferByteAccess
+    {
+        void GetBuffer(out byte* buffer, out uint capacity);
+    }
+
+    internal static class QRDecodeHelper
+    {
+        public async static Task<string> GetQRInfoFromBitmap(RandomAccessStreamReference bitmap)
+        {
+            IRandomAccessStream stream = await bitmap.OpenReadAsync();
+            BitmapDecoder decoder = await BitmapDecoder.CreateAsync(stream);
+            SoftwareBitmap softwareBitmap = await decoder.GetSoftwareBitmapAsync();
+
+            OpenCvSharp.Mat mat = SoftwareBitmap2Mat(softwareBitmap);
+
+            OpenCvSharp.QRCodeDetector qRCodeDetector = new OpenCvSharp.QRCodeDetector();
+            OpenCvSharp.Point2f[] points;
+            string qrResult = qRCodeDetector.DetectAndDecode(mat, out points);
+
+            return qrResult;
+        }
+
+        // https://github.com/shimat/opencvsharp_samples/blob/master/CameraOpenCV/OcvOp.cs
+        public static unsafe OpenCvSharp.Mat SoftwareBitmap2Mat(SoftwareBitmap softwareBitmap)
+        {
+            using (BitmapBuffer buffer = softwareBitmap.LockBuffer(BitmapBufferAccessMode.Write))
+            {
+                using (var reference = buffer.CreateReference())
+                {
+                    ((IMemoryBufferByteAccess)reference).GetBuffer(out var dataInBytes, out var capacity);
+
+                    OpenCvSharp.Mat outputMat = new OpenCvSharp.Mat(softwareBitmap.PixelHeight, softwareBitmap.PixelWidth, OpenCvSharp.MatType.CV_8UC4, (IntPtr)dataInBytes);
+                    return outputMat;
+                }
+            }
+        }
+    }
+}
